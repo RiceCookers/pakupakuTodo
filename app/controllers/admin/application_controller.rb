5448ebf063c65c2f2f24ea519e73acb85e937efc
kazumalab <kazuma-arashi@hotmail.co.jp>
2018-09-16 14:53:06 +0900

    add: admin

---
+++ app/controllers/admin/application_controller.rb
@@ -0,0 +1,22 @@
+class Admin::ApplicationController < ActionController::Base
+  helper_method :current_user, :signed_in?
+  layout "admin/application"
+
+  private
+
+    def sign_in(admin_user)
+      session[:admin_user_id] = admin_user.id
+    end
+
+    def signed_in?
+      current_user.present?
+    end
+
+    def current_user
+      @current_user ||= Admin::User.find_by(id: session[:admin_user_id])
+    end
+
+    def require_signed_in
+      redirect_to new_user_path unless signed_in?
+    end
+end
